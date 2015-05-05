6246023f564b12e16a7641c5335d2ebaeee05c63
Ash Priom <sa.priom@gmail.com>
2015-05-05 19:57:23 -0400

    SOEN 6011 - DevProcess
    Concentration on discrete mathematics.

---
+++ src/devprocess/DevProcess.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devprocess;
+
+/**
+ * @author Priom
+ *
+ */
+public class DevProcess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the matrix.");
+	}
+
+}
